b9097948777fa4c79c98e6f86fe11db67a787129
pr4j3sh <prajesh.eleven118@gmail.com>
2025-04-27 17:55:32 +0530

    docs: updated README

--- src/components/protected.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-export default function Protected({ children }) {
-  const user = useSelector((state) => state.user.uid);
-
-  return user ? children : <Navigate to="/" replace />;
-}
--- src/pages/dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import Crud from "../components/crud";
-
-export default function Dashboard() {
-  return (
-    <section>
-      <Crud />
-    </section>
-  );
-}
